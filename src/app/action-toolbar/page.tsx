cb3fd54ce0350a4ee9b786e08a63cb7e9a4f4618
RohanJacob23 <rohankoshyjacob@gmail.com>
2024-06-25 14:36:46 +0530

    Completed action toolbar

--- src/app/action-toolbar/page.tsx
+++ src/app/action-toolbar/page.tsx
@@ -5,7 +5,7 @@ export default function page() {
     <main className="flex flex-col px-2.5 py-8 md:px-8">
       <h2 className="border-none text-center underline">Action Toolbar</h2>
 
-      <section className="mt-4 flex min-h-[50vh] flex-col items-center justify-center">
+      <section className="mt-4 flex min-h-[50vh] flex-col items-center justify-evenly">
         <ActionToolbar />
       </section>
     </main>
